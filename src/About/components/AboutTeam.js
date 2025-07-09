ecc10ad8b0e3940b8af03df05f3315426898445f
QudratUllahKhan9 <qudratkhan0409@gmail.com>
2025-07-09 06:17:48 -0700

    new

--- src/About/components/AboutTeam.js
+++ src/About/components/AboutTeam.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import './AboutSection.css';
-import teamImage from '../asstes/about-full.jpeg'; // replace with your actual image path
+import React from "react";
+import "./AboutSection.css";
+import teamImage from "../asstes/about-full.jpeg"; // replace with your actual image path
 
 const AboutSection = () => {
   return (
     <div className="about-container">
       <div className="image-wrapper">
-        <img src={teamImage} alt="Standard Arabia Team" className="team-image" />
+        <img
+          src={teamImage}
+          alt="Standard Arabia Team"
+          className="team-image"
+        />
       </div>
       <div className="text-section">
         <p className="about-label">ABOUT US</p>
         <h2 className="about-title">We Are Beyond</h2>
         <p className="about-description">
-          Standard Arabia Inspection Co. Ltd has been established in 2015 at Al Jubail – Saudi Arabia. We specialize in
-          Third-Party Inspection, Safety and Operator Training and Certification, Advance and conventional NDT services,
-          Marine Services, Material testing and Calibration with divisions names as Standard Arabia Training Center and
-          Standard Arabia testing and Calibration. We have branches in all major cities of the country/GCC including – Al
-          Khobar, Riyadh, Jeddah, Yanbu, Turaif, Shuqaiq, Jizan, Tabuk & Bahrain.
+          Standard Arabia Inspection Co. Ltd has been established in 2015 at Al
+          Jubail – Saudi Arabia. We specialize in Third-Party Inspection, Safety
+          and Operator Training and Certification, Advance and conventional NDT
+          services, Marine Services, Material testing and Calibration with
+          divisions names as Standard Arabia Training Center and Standard Arabia
+          testing and Calibration. We have branches in all major cities of the
+          country/GCC including – Al Khobar, Riyadh, Jeddah, Yanbu, Turaif,
+          Shuqaiq, Jizan, Tabuk & Bahrain.
+        </p>
+      </div>
+      <div>
+        <p className="about-description">
+          By providing modern Inspection and training solutions, we seek to
+          create an environment where everyone can equally reap the benefits of
+          an efficient and secure workplace. Our services guarantee total
+          compliance with international standards and affirms the mechanical
+          capacity of the machinery. We offer high quality interactive training
+          programs at customer sites, rigs and at our in-house training
+          facilities at Al Khobar, Jubail and Manama.
         </p>
       </div>
     </div>
--- src/About/components/Timeline.js
+++ src/About/components/Timeline.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Timeline.css";
 import image1 from '../asstes/2015.webp'
+import image2 from '../asstes/2016.webp'
+import image3 from '../asstes/2017.webp'
+import image4 from '../asstes/2018.webp'
+import image5 from '../asstes/2019.webp'
+import image6 from '../asstes/2020.webp'
+import image7 from '../asstes/2021.webp'
+import image8 from '../asstes/2022.webp'
+import image9 from '../asstes/2023.webp'
+
+
 const timelineData = [
   {
     year: "2015",
@@ -13,16 +23,55 @@ const timelineData = [
     year: "2016",
     title: "Operations",
     description: "Operations Started in CENTRAL, WESTERN & NORTHERN Province",
-    image: image1,
+    image: image2,
     // align: "right"
   },
   {
     year: "2017",
     title: "Contract",
     description: "MAADEN Long Term Contract Awarded",
-    image: image1,
+    image: image3,
     // align: "left"
-  }
+  },
+    {
+    year: "2018",
+    title: "New Department",
+    description: "NDT Department was Established",
+    image: image4,
+    // align: "left"
+  },  {
+    year: "2019",
+    title: "Sabic Agreement",
+    description: "Sabic Master AGREEMENT Awarded and Established Testing and Calibration Dept",
+    image: image5,
+    // align: "left"
+  },
+   {
+    year: "2020",
+    title: "Long Term Contract",
+    description: "Long Term Contract awarded in Yanbu Region and MARAFIQ outline agreement for Training",
+    image: image6,
+    // align: "left"
+  }, {
+    year: "2021",
+    title: "Approved from IPAF AND Food Safety",
+    description: "Marine & Coating Dept. started IPAF & Food Safety Approval",
+    image: image7,
+    // align: "left"
+  }, {
+    year: "2022",
+    title: "Established Testing Laboratory",
+    description: "Established new laboratory for testing department.",
+    image: image8,
+    // align: "left"
+  }, {
+    year: "2023",
+    title: "Established Branch Office in NEOM",
+    description: "Establishes a new branch in NEOM for the vision 2030.",
+    image: image9,
+    // align: "left"
+  },
+  
 ];
 
 const Timeline = () => {
